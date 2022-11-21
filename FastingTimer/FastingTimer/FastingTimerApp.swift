5425b3ec15e193dd68e70bc05284d2b799aa39e1
LinaVertinskaya <linavertinskayaa@gmail.com>
2022-11-21 15:44:23 +0300

    Add my project

---
+++ FastingTimer/FastingTimer/FastingTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastingTimerApp.swift
+//  FastingTimer
+//
+//  Created by Лина Вертинская on 21.11.22.
+//
+
+import SwiftUI
+
+@main
+struct FastingTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
